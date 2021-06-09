aeaa3bec8639d59f810468045080f5c3fcf9ba95
ustdevops05 <ustdevops05@iiht.tech>
2021-06-09 20:15:21 +0530

    first commit

---
+++ Application/src/main/java/com/main/Application/MyAppController.java
@@ -0,0 +1,16 @@
+package com.main.Application;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class MyAppController {
+    
+    @RequestMapping("/")
+    public String index() {
+        return "index.html";
+    }
+    
+}
